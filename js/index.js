83293ec4640be966b2bebfdaff6277720386806e
mariam-farrukh <50977034+mariam-farrukh@users.noreply.github.com>
2019-06-18 14:53:30 -0500

    Created three different event listeners

--- js/index.js
+++ js/index.js
@@ -1,1 +1,20 @@
 // Your code goes here
+
+//increase size of logo with mouseover
+const navLogo = document.querySelector('.logo-heading');
+navLogo.addEventListener("mouseover", (event) => {
+    event.target.style.transform = "scale(1.25)"});
+
+//Changes background color of nav links when double clicked.
+const nav = document.querySelector(".nav");
+nav.addEventListener("dblclick", (event) => {
+    event.target.style.backgroundColor = '#17A2B8';
+    event.target.style.borderRadius = '3px';
+    event.target.style.color = 'white';
+});
+
+//changes header opacity with any key pressed.
+const body = document.querySelector('body');
+const header = document.querySelector('header');
+body.addEventListener("keydown", () => {
+    header.style.opacity = "0.8"});
